1dcc66e18ff5048eaefb6368306e06c74ad34496
BeanZ <92383674+BeanZ141@users.noreply.github.com>
2024-02-10 17:04:23 +0530

    Add files via upload

---
+++ BlackScreenPopup/BlackScreenPopup/CustomSlider.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class CustomSlider : UserControl
+{
+    private int _minimum = 0;
+    private int _maximum = 100;
+    private int _value = 0;
+
+    public int Minimum
+    {
+        get { return _minimum; }
+        set { _minimum = value; Invalidate(); }
+    }
+
+    public int Maximum
+    {
+        get { return _maximum; }
+        set { _maximum = value; Invalidate(); }
+    }
+
+    public int Value
+    {
+        get { return _value; }
+        set { _value = Math.Max(Minimum, Math.Min(Maximum, value)); Invalidate(); }
+    }
+
+    public event EventHandler ValueChanged;
+
+    protected virtual void OnValueChanged(EventArgs e)
+    {
+        ValueChanged?.Invoke(this, e);
+    }
+
+    protected override void OnMouseDown(MouseEventArgs e)
+    {
+        base.OnMouseDown(e);
+        HandleMouseInput(e);
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        base.OnMouseMove(e);
+        if (e.Button == MouseButtons.Left)
+        {
+            HandleMouseInput(e);
+        }
+    }
+
+    private void HandleMouseInput(MouseEventArgs e)
+    {
+        float percent = (float)e.X / Width;
+        Value = (int)(percent * (Maximum - Minimum)) + Minimum;
+        OnValueChanged(EventArgs.Empty);
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        DrawSlider(e.Graphics);
+    }
+
+    private void DrawSlider(Graphics g)
+    {
+        int trackHeight = 1;
+        using (var trackBrush = new SolidBrush(ColorTranslator.FromHtml("#1C1C1C")))
+        {
+            g.FillRectangle(trackBrush, 0, Height / 2 - trackHeight / 2, Width, trackHeight);
+        }
+
+        float percent = (float)(Value - Minimum) / (Maximum - Minimum);
+        int thumbX = (int)(percent * Width);
+
+        int thumbWidth = 10;
+        using (var thumbBrush = new SolidBrush(ColorTranslator.FromHtml("#B9FF42")))
+        {
+            g.FillEllipse(thumbBrush, thumbX - thumbWidth / 2, Height / 2 - thumbWidth / 2, thumbWidth, thumbWidth);
+        }
+    }
+}
